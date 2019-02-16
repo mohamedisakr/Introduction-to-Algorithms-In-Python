95a2bf2a1800b0e2252a4f8fcc325832813ea6ff
Mohamed Sakr <mohamedisakr@gmail.com>
2019-02-16 17:44:02 +0200

    Update OddNextEven.py

--- OddNextEven.py
+++ OddNextEven.py
@@ -1,5 +1,15 @@
 def GetNextEven(odd):
-  
+    """ (int) -> int
+    Return the even number next to the odd number (parameter).
+    >>> GetNextEven(7)
+    8
+    >>> GetNextEven(3)
+    4
+    >>> GetNextEven(17)
+    18
+    >>> GetNextEven(31)
+    32
+    """  
     if odd % 2 != 0 : 
         nextEven = odd + 1
     return nextEven
